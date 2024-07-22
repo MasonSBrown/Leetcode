dc93cf79ef235034c67da2bf03a8f0ac7891aa08
Mason Brown <147461753+MasonSBrown@users.noreply.github.com>
2024-07-21 19:34:23 -0700

    Time: 161 ms (46.70%), Space: 28 MB (49.99%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,16 +1,25 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        if k > len(nums):
-            k = k % len(nums)
+        lng = len(nums)
+        if k > lng:
+            k %= lng
 
-        if k == 0 or len(nums) <= 1:
+        if k == 0 or lng <= 1:
             return nums
 
-        empty_list = [0 for _ in range(len(nums))]
+        tmp = [0] * lng
 
-        for i in range(len(nums)):
-            if i + k >= len(nums):
-                empty_list[(k-len(nums)+i)] = nums[i]
-            else:
-                empty_list[(i+k)] = nums[i]
-        nums[:] = empty_list
+        # [0 for _ in range(len(nums))]
+
+        # for i in range(len(nums)):
+        #     if i + k >= len(nums):
+        #         empty_list[(k-len(nums)+i)] = nums[i]
+        #     else:
+        #         empty_list[(i+k)] = nums[i]
+        # nums[:] = empty_list
+        
+        tmp = nums[lng-k:] + nums[:lng-k]
+        nums[:] = tmp
+
+
+
